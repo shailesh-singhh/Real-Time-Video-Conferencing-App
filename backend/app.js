5e6bb2cfb06f6ebb20d21ebffcc6eb58ea81e9eb
Shailesh <singhshailesh2901@gmail.com>
2025-08-25 23:34:02 +0530

    change directory

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 
 import mongoose from "mongoose";
-import { connectToSocket } from "./controllers/socketManager.js";
+import { connectToSocket } from "./src/controllers/socketManager.js";
 
 import cors from "cors";
-import userRoutes from "./routes/users.routes.js";
+import userRoutes from "./src/routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
